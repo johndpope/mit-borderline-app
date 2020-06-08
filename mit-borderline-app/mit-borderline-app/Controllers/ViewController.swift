44a38d051873a4e60c64ff1589e8e4a2a019b81c
Elaine Xiao <eyxiao@mit.edu>
2020-06-07 23:13:24 -0700

    Add image detection, video playing, and video looping in ViewController.swift. Update Info.plist to include camera privileges.

--- mit-borderline-app/mit-borderline-app/Controllers/ViewController.swift
+++ mit-borderline-app/mit-borderline-app/Controllers/ViewController.swift
@@ -8,8 +8,11 @@
 
 import UIKit
 import SwiftyGif
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
     
     let logoAnimationView = LogoAnimationView()
 
@@ -18,12 +21,97 @@ class ViewController: UIViewController {
         view.addSubview(logoAnimationView)
         logoAnimationView.pinEdgesToSuperView()
         logoAnimationView.logoGifImageView.delegate = self
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         logoAnimationView.logoGifImageView.startAnimatingGif()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARImageTrackingConfiguration()
+        
+        if let trackedImages = ARReferenceImage.referenceImages(inGroupNamed: "BorderlineImages", bundle: Bundle.main) {
+            configuration.trackingImages = trackedImages
+            
+            configuration.maximumNumberOfTrackedImages = 1
+            
+//            configuration.automaticImageScaleEstimationEnabled = true
+            
+        }
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+        
+        if let imageAnchor = anchor as? ARImageAnchor {
+            // Create a video player
+            let videoUrl = Bundle.main.url(forResource: "elisa_young", withExtension: "mp4")!
+            let videoPlayer = AVPlayer(url: videoUrl)
+
+            // To make the video loop
+            videoPlayer.actionAtItemEnd = .none
+            NotificationCenter.default.addObserver(
+                self,
+                selector: #selector(ViewController.playerItemDidReachEnd),
+                name: NSNotification.Name.AVPlayerItemDidPlayToEndTime,
+                object: videoPlayer.currentItem)
+            
+            let videoNode = SKVideoNode(avPlayer: videoPlayer)
+            
+            videoNode.play()
+            
+            let videoScene = SKScene(size: CGSize(width: 600, height: 600))
+            
+            videoNode.position = CGPoint(x: videoScene.size.width/2, y: videoScene.size.height/2) // centers video
+            videoNode.yScale = -1.0 // flips video to correct orientation
+            
+            videoScene.addChild(videoNode)
+            
+            let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
+            
+            plane.firstMaterial?.diffuse.contents = videoScene
+            
+            let planeNode = SCNNode(geometry: plane)
+            
+            planeNode.eulerAngles.x = -.pi / 2
+            
+            node.addChildNode(planeNode)
+            
+        }
+        return node
+    }
+    
+    
+    // This callback will restart the video when it has reach its end
+    @objc func playerItemDidReachEnd(notification: NSNotification) {
+        if let playerItem: AVPlayerItem = notification.object as? AVPlayerItem {
+            playerItem.seek(to: CMTime.zero)
+        }
+    }
+    
 }
 
 extension ViewController: SwiftyGifDelegate {
