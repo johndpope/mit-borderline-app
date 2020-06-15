04a2bbabbbd227b5223ca125cf6f8ee564750d19
Elaine Xiao <eyxiao@mit.edu>
2020-06-14 21:05:19 -0700

    Add green screen video and chroma key code
    * Need to fix chroma key code to make pixels transparent instead of black

--- mit-borderline-app/mit-borderline-app/Controllers/ViewController.swift
+++ mit-borderline-app/mit-borderline-app/Controllers/ViewController.swift
@@ -84,7 +84,14 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             // Rescale and center videoNode
             rescaleVideoNode(videoNode: videoNode, sceneWidth: videoScene.size.width, sceneHeight: videoScene.size.height)
-            videoScene.addChild(videoNode)
+            
+            // Create effect node to make background transparent
+            let effectNode = SKEffectNode()
+            effectNode.addChild(videoNode)
+            effectNode.filter = colorCubeFilterForChromaKey(hueAngle: 120)
+            
+            // Add effect node to videoScene
+            videoScene.addChild(effectNode)
             
             // Create plane for videoScene
             let plane = SCNPlane(width: imageAnchor.referenceImage.physicalSize.width, height: imageAnchor.referenceImage.physicalSize.height)
@@ -116,6 +123,56 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         if let playerItem: AVPlayerItem = notification.object as? AVPlayerItem {
             playerItem.seek(to: CMTime.zero)
         }
+    }
+    
+    func RGBtoHSV(r : Float, g : Float, b : Float) -> (h : Float, s : Float, v : Float) {
+        var h : CGFloat = 0
+        var s : CGFloat = 0
+        var v : CGFloat = 0
+        let col = UIColor(red: CGFloat(r), green: CGFloat(g), blue: CGFloat(b), alpha: 1.0)
+        col.getHue(&h, saturation: &s, brightness: &v, alpha: nil)
+        return (Float(h), Float(s), Float(v))
+    }
+
+    func colorCubeFilterForChromaKey(hueAngle: Float) -> CIFilter {
+
+        let hueRange: Float = 20 // degrees size pie shape that we want to replace
+        let minHueAngle: Float = (hueAngle - hueRange/2.0) / 360
+        let maxHueAngle: Float = (hueAngle + hueRange/2.0) / 360
+
+        let size = 64
+        var cubeData = [Float](repeating: 0, count: size * size * size * 4)
+        var rgb: [Float] = [0, 0, 0]
+        var hsv: (h : Float, s : Float, v : Float)
+        var offset = 0
+
+        for z in 0 ..< size {
+            rgb[2] = Float(z) / Float(size) // blue value
+            for y in 0 ..< size {
+                rgb[1] = Float(y) / Float(size) // green value
+                for x in 0 ..< size {
+
+                    rgb[0] = Float(x) / Float(size) // red value
+                    hsv = RGBtoHSV(r: rgb[0], g: rgb[1], b: rgb[2])
+                    // TODO: Check if hsv.s > 0.5 is really nesseccary
+                    let alpha: Float = (hsv.h > minHueAngle && hsv.h < maxHueAngle && hsv.s > 0.5) ? 0 : 1.0
+
+                    cubeData[offset] = rgb[0] * alpha
+                    cubeData[offset + 1] = rgb[1] * alpha
+                    cubeData[offset + 2] = rgb[2] * alpha
+                    cubeData[offset + 3] = alpha
+                    offset += 4
+                }
+            }
+        }
+        let b = cubeData.withUnsafeBufferPointer { Data(buffer: $0) }
+        let data = b as NSData
+
+        let colorCube = CIFilter(name: "CIColorCube", parameters: [
+            "inputCubeDimension": size,
+            "inputCubeData": data
+            ])
+        return colorCube!
     }
     
     
